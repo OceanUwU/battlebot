69c1abba6b99f1162b5274b3c8e721f22fcb65f7
Ocean <s@ocean.lol>
2021-08-14 20:30:48 +0100

    f

--- fn/isMod.js
+++ fn/isMod.js
@@ -1,6 +1,6 @@
 const cfg = require('../cfg.json');
 
-async function isMod(interaction) {
+function isMod(interaction) {
     return interaction.member.permissions.has('MANAGE_GUILD') || cfg.overriders.includes(interaction.user.id);
 }
 
