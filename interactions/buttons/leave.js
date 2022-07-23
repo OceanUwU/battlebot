27cfbe7fd3d6ee6b5b35bc399e81b6fdd52ba572
Ocean <s@ocean.lol>
2022-07-23 18:17:41 +0100

    the "saint jerry" incident

--- interactions/buttons/leave.js
+++ interactions/buttons/leave.js
@@ -2,16 +2,15 @@ const db = require.main.require('./models');
 const editPlayerList = require.main.require('./fn/editPlayerList.js');
 
 module.exports = async interaction => {
-    if (interaction.message.content.includes(interaction.user.id)) {
-        let game = await db.Game.findOne({where: {channel: interaction.channelId}});
-        if (!game)
-            return interaction.reply({content: 'Couldn\'t find this game.', ephemeral: true});
-        if (game.started)
-            return interaction.reply({content: 'This game has already started.', ephemeral: true});
-        await db.Player.destroy({where: {game: game.id, user: interaction.user.id}});
-        //remove player from player list in message
-        await editPlayerList(game, interaction.message);
-        await interaction.reply({content: `<@${interaction.user.id}> left the game!`, allowedMentions: {users: []}});
-    } else
-        await interaction.reply({content: 'You need to join the game to leave it!', ephemeral: true});
+    let game = await db.Game.findOne({where: {channel: interaction.channelId}});
+    if (!game)
+        return interaction.reply({content: 'Couldn\'t find this game.', ephemeral: true});
+    if (game.started)
+        return interaction.reply({content: 'This game has already started.', ephemeral: true});
+    if (await db.Player.count({where: {game: game.id, user: interaction.user.id}}) < 1)
+        return interaction.reply({content: 'You need to join the game to leave it!', ephemeral: true});
+    await db.Player.destroy({where: {game: game.id, user: interaction.user.id}});
+    //remove player from player list in message
+    await editPlayerList(game, interaction.message);
+    await interaction.reply({content: `<@${interaction.user.id}> left the game!`, allowedMentions: {users: []}});
 };
