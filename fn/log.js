7da0158ec4b39e12cdc6573f9e905be37a68a790
Ocean <s@ocean.lol>
2022-07-26 00:17:01 +0100

    u can no longer crash the bot by deleting a channel

--- fn/log.js
+++ fn/log.js
@@ -2,8 +2,8 @@ const renderBoard = require('./renderBoard');
 const bot = require('../');
 
 async function log(game, text, showBoard=true, mentions=[]) {
-    let channel = await bot.channels.fetch(game.channel);
-    await channel.send({content: text, allowedMentions: {users: mentions}, files: showBoard ? [await renderBoard(game)] : []});
+    let channel = await bot.channels.fetch(game.channel).catch(e => {});
+    await channel?.send({content: text, allowedMentions: {users: mentions}, files: showBoard ? [await renderBoard(game)] : []});
 }
 
 module.exports = log;
