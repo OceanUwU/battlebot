404fcd1c395d96a1ab34d613fa9efe7ff02fde40
Ocean <s@ocean.lol>
2022-08-16 02:41:36 +0100

    cant join if started

--- interactions/commands/join.js
+++ interactions/commands/join.js
@@ -13,6 +13,8 @@ module.exports = {
         let game = await db.Game.findOne({where: {channel: interaction.channelId}});
         if (!game)
             return interaction.reply({content: 'Couldn\'t find this game.', ephemeral: true});
+        if (game.started)
+            return interaction.reply({content: 'This game has already started.', ephemeral: true});
         await interaction.reply({content: 'Join using the link below:', ephemeral: interaction.options.getBoolean('send') !== true, components: [
             new MessageActionRow().addComponents(new MessageButton().setLabel('Join').setStyle('LINK').setURL(`https://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${game.joinMenu}`))
         ]});
