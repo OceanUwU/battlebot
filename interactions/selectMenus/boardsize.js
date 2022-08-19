da0d3f37e8667a3638505ef1e77086cbe63bcb65
Ocean <s@ocean.lol>
2022-08-19 17:18:18 +0100

    cant change board size after start

--- interactions/selectMenus/boardsize.js
+++ interactions/selectMenus/boardsize.js
@@ -7,6 +7,8 @@ module.exports = async interaction => {
         return interaction.reply({content: 'Couldn\'t find this game.', ephemeral: true});
     if (!isMod(interaction))
         return interaction.reply({content: 'You must have the Manage Server permission to change this.', ephemeral: true});
+    if (!game.started)
+        return interaction.reply({content: 'You can\'t change this after the game has started.', ephemeral: true});
     let [width, height] = interaction.values[0].split('x').map(n => Number(n));
     await game.update({width, height});
     await interaction.reply({content: `<@${interaction.user.id}> changed the board size to ${game.width}x${game.height}.`, allowedMentions: {users: []}});
