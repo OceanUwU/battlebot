51dfae1d17155e1e8f436d29901e1817493f05e8
Ocean <s@ocean.lol>
2022-12-22 00:35:50 +0000

    Update log.js

--- models/game/log.js
+++ models/game/log.js
@@ -7,7 +7,7 @@ db.Game.prototype.log = async function(text, showBoard=true, mentions=[]) {
         allowedMentions: {users: mentions},
         files: showBoard ? [await this.render()] : []
     }).catch(e => {});
-    if (showBoard)
+    if (showBoard && msg)
         await db.Board.create({game: this.id, file: msg.attachments.first().id});
     return msg;
 };
