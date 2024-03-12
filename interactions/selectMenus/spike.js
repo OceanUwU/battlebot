0beeed261039e996039208ac0046a0470774f520
ocean <ocean@ocean.lol>
2024-03-12 09:40:52 +0000

    spike selection

---
+++ interactions/selectMenus/spike.js
@@ -0,0 +1,9 @@
+const db = require.main.require('./models');
+const playerOnly = require.main.require('./fn/playerOnly.js');
+
+module.exports = async interaction => {
+    let [game, player] = await playerOnly(interaction);
+    if (game == null) return;
+    await db.Player.update({spikeSelected: Number(interaction.values[0])}, {where: {id: player.id}});
+    await interaction.deferUpdate();
+};
